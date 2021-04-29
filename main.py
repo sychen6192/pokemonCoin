d0988c1dd840e9f6b005ee0c75358f77d0c31b31
sychen6192 <qazxsw9295@gmail.com>
2021-04-29 21:27:56 +0800

    [modify] hanged refresh

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import pyautogui, time
 def catchImageClick(img):
     pyautogui.click((pyautogui.locateCenterOnScreen(img)))
 
+
 def getPosition(img):
     s = pyautogui.locateOnScreen(img, confidence=0.9)
     pyautogui.moveTo(s.left+s.width//2, s.top+s.height//2)
-
     return s.left+s.width//2, s.top+s.height//2
+
 
 def moveTo(x, y):
     pyautogui.moveTo(x, y)
@@ -21,12 +22,12 @@ def drag(x, y):
 if __name__ == '__main__':
     while True:
         x_org, y_org = getPosition('./image/poke.png')
-        print(x_org, y_org)
+        print(time.time())
 
         catchImageClick('./image/battle.png')
 
         while not (pyautogui.locateOnScreen('./image/ad.png') or pyautogui.locateOnScreen('./image/ok.png')):
-            moveTo(x_org+20, y_org)
+            moveTo(x_org + 20, y_org)
             drag(0, -100)
 
         if pyautogui.locateOnScreen('./image/ad.png'):
@@ -34,9 +35,13 @@ if __name__ == '__main__':
             time.sleep(90)
             catchImageClick('./image/closeAd.png')
 
-
         else:
             catchImageClick('./image/ok.png')
 
+        count = 0
         while not (pyautogui.locateOnScreen('./image/poke.png')):
             time.sleep(1)
+            count += 1
+            if count > 10:
+                pyautogui.press('f5')
+                time.sleep(60)
